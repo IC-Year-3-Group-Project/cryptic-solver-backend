e2beaec6013b1047215d47cfa387661743c13b59
Zlatina Mileva <zlatinkaaaaaaaaaa@gmail.com>
2021-10-13 12:51:26 +0300

    [ljn19, ztm19] Created a function to match solutions with patterns of known letters and made a new endpoint for solving with pattern.

--- cryptic_solver/urls.py
+++ cryptic_solver/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from cryptic_solver.views import * 
 
 urlpatterns = [
-    path('solve-clue', solve_clue)
+    path('solve-clue', solve_clue),
+    path('solve-with-pattern', solve_with_pattern)
 ]   
--- cryptic_solver/views.py
+++ cryptic_solver/views.py
@@ -12,6 +12,13 @@ def option_response():
     response['Access-Control-Max-Age'] = 86400
     return response
 
+"""
+{
+    (String) 'clue'
+    (int)    'word_length'
+}
+"""
+
 @csrf_exempt
 def solve_clue(request):
     if request.method == 'OPTIONS':
@@ -20,3 +27,36 @@ def solve_clue(request):
         responses = requests.post("endpoint", data = request)
 
         return JsonResponse(responses, safe=False)
+        
+"""
+{
+    (String) 'clue'
+    (int)    'word_length'
+    (dict)   'pattern' e.g (index 3 -> 'e', index 5 -> 'a')  == ___E_A
+}
+"""
+
+@csrf_exempt
+def solve_with_pattern(request):
+    if request.method == 'OPTIONS':
+        return option_response()
+    else:
+        responses = requests.post("endpoint", data = request)
+        pattern = json.loads(request.body)['pattern']
+
+        return JsonResponse(matching(pattern, responses), safe=False)
+
+def matching(pattern, responses):
+    result = []
+    for response in responses:
+        match = True
+        for k,v in pattern.items():
+            if (response[k] != v):
+                match = False
+                break
+        if (match):
+            result.append(response)
+    return result
+
+            
+
