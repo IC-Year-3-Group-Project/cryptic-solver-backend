1c36f5791866879e6c1b52f5121b39834cfb4f0c
ztm19 <67147682+ztm19@users.noreply.github.com>
2021-10-13 13:53:08 +0300

    [ljn19, ztm19] Create matching function and add unit tests. (#8)
    * [ljn19, ztm19] Writing unit tests for the matching function and extracting it in a file for helper functions.
    
    * Fix a problem with import of helper module.

--- cryptic_solver/tests.py
+++ cryptic_solver/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from cryptic_solver.helper import matching
+import unittest
 
+class MatchingTests(unittest.TestCase):
+    def testOneSolution(self):
+        pattern = {0: 'A', 2:'E', 4:'A'} #A_E_A__
+        list = ['AVERAGE', 'ACADEMY', 'ADDRESS', 'ACCUSED', 'ABILITY']
+
+        self.assertEqual(matching(pattern, list), ['AVERAGE'])
+
+
+    def testMoreSolutions(self):
+        pattern = {0: 'B', 2:'B', 4:'E'} #B_B_E_
+        list = ['BABBLE', 'BABIED', 'BABIES', 'BABOON']
+
+        self.assertEqual(matching(pattern, list), ['BABIED', 'BABIES'])
+
+if __name__ == '__main__':
+    unittest.main()
+    
 # Create your tests here.
