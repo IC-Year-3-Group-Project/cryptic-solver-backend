f6f673e83b92e6468faac0f0342de71c57e9c974
Patrick Lotery <pdl119@ic.ac.uk>
2021-10-15 10:10:11 +0100

    [pdl] added clearer comments to haskell_interface

--- cryptic_solver/haskell_interface.py
+++ cryptic_solver/haskell_interface.py
@@ -3,7 +3,30 @@ import urllib.parse
 
 haskellURL = "http://84.238.224.41:5001"
 
-# calls out to the Haskell server and returns
+"""
+
+Arguments:
+
+(String) clue
+(int)    solutionLength
+
+(solve_with_answer)
+
+(String) answer
+
+(solve_with_pattern)
+
+(dict)   pattern
+
+
+Returns:
+
+(HTTPResponse) r
+    - the HTTP response from the Haskell server. This contains (as text)
+      all the solutions returned by the Haskell solver.
+
+"""
+
 def hs_solve_clue(clue, solutionLength):
     clue = urllib.parse.quote(clue, safe='')
 
@@ -36,10 +59,13 @@ def hs_solve_with_pattern(clue, solutionLength, pattern):
     return r
 
 
+# helper function to strip the [""] surrounding the response text
 def unlist(response):
     return response[2:-2]
 
+"""
 if __name__ == "__main__":
     hs_solve_clue("Peeling paint, profit slack, upset, in a state", 10)
     hs_solve_clue("Following kick-off, running back for corner", 4)
     hs_solve_with_answer("Peeling paint, profit slack, upset, in a state", 10, "california")
+"""
