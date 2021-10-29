eaf984f318bcab821ed73169132ce03861680553
Patrick Lotery <pdl119@ic.ac.uk>
2021-10-29 11:51:31 +0100

    [pdl] initial work for backend text recognition of clues - need to work on recognising columns to keep clues intact

---
+++ cryptic_solver/text_recognition.py
@@ -0,0 +1,11 @@
+import pytesseract
+import cv2
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread("../everyman 3907 clues.PNG")
+
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(img, config=custom_config)
+
+print(text)
