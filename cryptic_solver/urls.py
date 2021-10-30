b2b79913de82b0dbbbf5e7e2c22dcc854f49faaa
Xiaoning He <xhe123@hotmail.co.uk>
2021-10-30 15:37:26 +0100

    Add URL for explain_answer

--- cryptic_solver/urls.py
+++ cryptic_solver/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('solve-with-pattern', solve_with_pattern),
     path('fetch-crossword', fetch_crossword),
     path('solve-with-dict', solve_with_dict),
-    path('fetch-everyman', fetch_everyman)
+    path('fetch-everyman', fetch_everyman),
+    path('explain_answer', explain_answer)
 ]
