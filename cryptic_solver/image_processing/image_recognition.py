536f7a91eb99f50920a294c5db8c2b8120ec46c9
Zlatina Mileva <zlatina.mileva19@imperial.ac.uk>
2021-11-16 03:43:24 +0200

    Clean up functions, add consistent commenting in image_recognition and text_recognition.

--- cryptic_solver/image_processing/image_recognition.py
+++ cryptic_solver/image_processing/image_recognition.py
@@ -4,10 +4,23 @@ import base64
 from cryptic_solver.image_processing.grid_recognition import get_grid_from_image
 from cryptic_solver.image_processing.text_recognition import read_text
 
-# Combine grid and text recognition to produce one 
-# object with all crossword data
-def recognize_image(b64_grid, b64_across, b64_down):
 
+def recognize_image(b64_grid, b64_across, b64_down):
+    '''
+    Combine grid and text recognition to produce one 
+    object with all crossword data
+
+    Parameters: 
+    b64_grid: b64 representation of the grid image
+    b64_across: b64 representation of the across image
+    b64_down: b64 representation of the down image
+
+    Return:
+    grid_dict: Dictionary with the clues with their number,
+               direction, text, lenghts, total length 
+    '''
+
+    # Extraxt the image data matrices from the b64 representations
     grid = b64ToMatrix(b64_grid)
     across = b64ToMatrix(b64_across)
     down = b64ToMatrix(b64_down)
@@ -18,6 +31,7 @@ def recognize_image(b64_grid, b64_across, b64_down):
 
     grid_clues = grid_dict["clues"]
 
+    # Fill in the information from the recognized text in the clues
     for clue in down_dict:
       for grid_clue in grid_clues:
           if grid_clue["direction"] == 1 and grid_clue["number"] == clue["number"]:
@@ -34,8 +48,20 @@ def recognize_image(b64_grid, b64_across, b64_down):
 
     return grid_dict
 
+
 def b64ToMatrix(b64_image):
+    '''
+    Extract the image data matrix from the b64
+    representation of the image
+
+    Parameters: 
+    b64_image: b64 representation of the image
+
+    Return:
+    A matrix of the image data
+    '''
     im_bytes = base64.b64decode(b64_image)
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8) 
+
     return cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
 
--- cryptic_solver/image_processing/text_recognition.py
+++ cryptic_solver/image_processing/text_recognition.py
@@ -1,21 +1,22 @@
 import pytesseract
 import cv2
 import re
-
 from cryptic_solver_project import settings
 
 if settings.tesseract_cmd != "":
     pytesseract.pytesseract.tesseract_cmd = settings.tesseract_cmd
 
-def preprocess(image):
-
-    image = get_grayscale(image)
-    #image = remove_noise(image)
-    #image = thresholding(image)
-
-    return image
 
 def preprocess_text(text):
+    '''
+    Changes the misrecognized characters to the expected ones
+
+    Parameters: 
+    text: String
+
+    Return:
+    replaced_text: String
+    '''
     character_replacements = {"°": " ", "©": "(5)", "|": "I", "®": "(5", "@": "(4)"}
 
     replaced_text = text
@@ -34,37 +35,22 @@ def preprocess_text(text):
     return replaced_text
 
 
-#thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
-
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def read_text(image_data):
-
-    processed = preprocess(image_data)
-
-    custom_config = r"--oem 3 --psm 6"
-    text = pytesseract.image_to_string(processed, config=custom_config)
-    preprocessed_text = preprocess_text(text)
-    clues = parse_ocr(preprocessed_text)
-    return clues
-
 def parse_ocr(text):
-
     """
-    the idea:
-        - when a line starts with a number, it"s a new clue (add the currently building clue to clues, make a new one)
+    Extract the clues from a given text
+    The idea:
+        - when a line starts with a number, it"s a new clue 
+          (add the currently building clue to clues, make a new one)
         - all text past that is the actual clue
-        - if line ends with a number (excluding brackets) then that line ending is the solution pattern
+        - if line ends with a number (excluding brackets) 
+          then that line ending is the solution pattern
         - replace . with ,
 
+    Parameters:
+    text: String
+
+    Return:
+    clues: An array of clues with their text, number, lengths    
     """
 
     clues = []
@@ -76,7 +62,8 @@ def parse_ocr(text):
         if len(words) == 0:
             continue
 
-        # when a line starts with a number, it"s a new clue (add the currently building clue to clues, make a new one)
+        # When a line starts with a number, it's a new clue 
+        # (add the currently building clue to clues, make a new one)
         if re.match(r"\d+.*", words[0]) and len(words) > 1:
             if clue:
                 clue["text"] = clue_text.strip()
@@ -86,7 +73,8 @@ def parse_ocr(text):
             clue["number"] = get_int(words[0])
             del words[0]
 
-        # if line ends with a number (excluding brackets) then that line ending is the solution pattern
+        # If line ends with a number (excluding brackets) 
+        # then that line ending is the solution pattern
         if re.match("(\()?\d+((\.,-)?\d+)*(\))?", words[-1]):
             lengths_string = words[-1].replace(".", ",")[1:-1]
             lengths = re.split(",|-", lengths_string)
@@ -99,9 +87,52 @@ def parse_ocr(text):
     return clues
 
 
+def read_text(image_data):
+    '''
+    Extract the text from the given image
+
+    Parameters: 
+    image_data: The image data array (ndarray)
+
+    Return:
+    clues: An array of clues with their text, number, lengths
+    '''
+    gray = get_grayscale(image_data)
+
+    custom_config = r"--oem 3 --psm 6"
+    text = pytesseract.image_to_string(gray, config=custom_config)
+    preprocessed_text = preprocess_text(text)
+    clues = parse_ocr(preprocessed_text)
+
+    return clues
+
+
 def get_int(word):
+    '''
+    Extract a number as int from the beginning of a string
+
+    Parameters: 
+    word: String
+
+    Return:
+    (int): The integer at the beginning of word
+    '''
+
     result = ""
     for char in word:
-        if char.isnumeric():
+        if char.isnumeri():
             result += char
+
     return int(result)
+
+# Thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# Noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+# Get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
