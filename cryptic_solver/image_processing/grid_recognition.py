bc0fcfc0cbefe6d9a329c2d9a6fdb94ef17ae043
Guolin Yang <guolin.yang13@gmail.com>
2021-11-17 14:40:44 +0000

    Tuned grid recognition

--- cryptic_solver/image_processing/grid_recognition.py
+++ cryptic_solver/image_processing/grid_recognition.py
@@ -11,29 +11,29 @@ epsilon = 0.0005
 
 
 def get_grid_from_image(image):
-    '''
+    """
     Get the grid in a dictionary format from an image
 
-    Parameters: 
+    Parameters:
     image: Image data array (ndarray)
 
     Return:
     grid_as_json: Dictionary with fields:
                   clues: Array of clues
-                  rows: int 
+                  rows: int
                   columns: int
-    '''
+    """
     image = process_grid_image(image)
 
     # Convert RGB to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Threshold image to black and white
-    _, thresh = cv2.threshold(gray, 215, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(thresh, (5, 5), 0)
 
     # Find contours
-    contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     contour_list = []
 
@@ -53,16 +53,24 @@ def get_grid_from_image(image):
         contour_list.append(contour)
 
         median_area = sorted(map(cv2.contourArea, contour_list))[
-            math.floor(len(contour_list) / 2)]
+            math.floor(len(contour_list) / 2)
+        ]
         median_perimeter = sorted([cv2.arcLength(c, True) for c in contour_list])[
-            math.floor(len(contour_list) / 2)]
+            math.floor(len(contour_list) / 2)
+        ]
 
     temp = []
     for contour in contour_list:
-        if abs(cv2.contourArea(contour) - median_area) > median_area / 2.0 and \
-                abs(cv2.arcLength(contour, True) - median_perimeter) < median_perimeter / 2.0:
+        if (
+            abs(cv2.contourArea(contour) - median_area) > median_area / 2.0
+            and abs(cv2.arcLength(contour, True) - median_perimeter)
+            < median_perimeter / 2.0
+        ):
             continue
-        if abs(cv2.arcLength(contour, True) - median_perimeter) >= median_perimeter / 2.0:
+        if (
+            abs(cv2.arcLength(contour, True) - median_perimeter)
+            >= median_perimeter / 2.0
+        ):
             continue
 
         temp.append(contour)
@@ -103,11 +111,11 @@ def get_grid_from_image(image):
 
 
 def create_grid(x_bounds, y_bounds, rectangles):
-    '''
+    """
     Initialize the grid as an array containing the
     location and sizes of the cells
 
-    Parameters: 
+    Parameters:
     x_bounds: Array of x-coordinates of the cells (ndarray)
     y_bounds: Array of y-coordinates of the cells (ndarray)
     rectangles: The cells with their x, y, width and height (ndarray)
@@ -116,7 +124,7 @@ def create_grid(x_bounds, y_bounds, rectangles):
     grid: Array with the same structure as the grid
           that has a dictionary field for each cell
 
-    '''
+    """
     x_bounds_len = len(x_bounds)
     y_bounds_len = len(y_bounds)
 
@@ -127,10 +135,10 @@ def create_grid(x_bounds, y_bounds, rectangles):
         k = 0
 
         x, y, _, _ = rectangles[i]
-        while j < y_bounds_len-1 and y_bounds[j + 1] <= y + epsilon:
+        while j < y_bounds_len - 1 and y_bounds[j + 1] <= y + epsilon:
             j = j + 1
 
-        while k < x_bounds_len-1 and x_bounds[k + 1] <= x + epsilon:
+        while k < x_bounds_len - 1 and x_bounds[k + 1] <= x + epsilon:
             k = k + 1
 
         grid[j][k] = {"can_go_to": []}
@@ -139,14 +147,14 @@ def create_grid(x_bounds, y_bounds, rectangles):
 
 
 def fill_clue_numbers(grid):
-    '''
+    """
     Fill clue numbers for the cells that should contain
     a number
 
-    Parameters: 
+    Parameters:
     grid: An array representation of the grid with dictionary
           fields for each cell
-    '''
+    """
 
     clue_number = 1
     m, n = np.shape(grid)
@@ -156,17 +164,17 @@ def fill_clue_numbers(grid):
             if grid[j][k] == None:
                 continue
 
-            if (j + 1 < m and grid[j + 1][k] != None):
+            if j + 1 < m and grid[j + 1][k] != None:
                 grid[j + 1][k].get("can_go_to").append("top")
                 grid[j][k].get("can_go_to").append("bottom")
 
-            if (k + 1 < n and grid[j][k + 1] != None):
+            if k + 1 < n and grid[j][k + 1] != None:
                 grid[j][k + 1].get("can_go_to").append("left")
                 grid[j][k].get("can_go_to").append("right")
 
             can_go_to = grid[j][k].get("can_go_to")
 
-            # Assign numbers to the cells from which you can only go 
+            # Assign numbers to the cells from which you can only go
             # down or right
             is_clue = False
             if not "top" in can_go_to and "bottom" in can_go_to:
@@ -179,36 +187,36 @@ def fill_clue_numbers(grid):
                 grid[j][k]["clue_number"] = clue_number
                 is_clue = True
 
-            if (is_clue):
+            if is_clue:
                 clue_number = clue_number + 1
 
 
 def fill_clue_lengths(grid):
-    '''
+    """
     Fill clue lengths from the bottom right to the top left
     according to whether they are going down or accross
 
-    Parameters: 
+    Parameters:
     grid: An array representation of the grid with dictionary
           fields for each cell
-    '''
+    """
 
     m, n = np.shape(grid)
 
-    for j in range(m-1, -1, -1):
-        for k in range(n-1, -1, -1):
-            if (grid[j][k] == None):
+    for j in range(m - 1, -1, -1):
+        for k in range(n - 1, -1, -1):
+            if grid[j][k] == None:
                 continue
 
             down = 1
             right = 1
 
-            if (j + 1 < m):
-                if (grid[j + 1][k] != None):
+            if j + 1 < m:
+                if grid[j + 1][k] != None:
                     down += grid[j + 1][k]["down"]
 
-            if (k + 1 < n):
-                if (grid[j][k + 1] != None):
+            if k + 1 < n:
+                if grid[j][k + 1] != None:
                     right += grid[j][k + 1]["right"]
 
             grid[j][k]["down"] = down
@@ -216,32 +224,32 @@ def fill_clue_lengths(grid):
 
 
 def get_grid_as_json(grid):
-    '''
+    """
     Create a JSON object from the grid
 
-    Parameters: 
+    Parameters:
     grid: An array representation of the grid with dictionary
           fields for each cell
 
     Return:
     grid_as_json: Dictionary with fields:
                   clues: (ndarray)
-                  rows: (int) 
-                  columns: (int) 
-    '''
+                  rows: (int)
+                  columns: (int)
+    """
 
     clues = []
     m, n = np.shape(grid)
 
-    for j in range(m-1, -1, -1):
-        for k in range(n-1, -1, -1):
-            if (grid[j][k] == None):
+    for j in range(m - 1, -1, -1):
+        for k in range(n - 1, -1, -1):
+            if grid[j][k] == None:
                 continue
 
-            if (grid[j][k]["down"] > 1 and "is_down" in grid[j][k]):
+            if grid[j][k]["down"] > 1 and "is_down" in grid[j][k]:
                 write_clue(grid, clues, j, k, "down")
 
-            if (grid[j][k]["right"] > 1 and "is_across" in grid[j][k]):
+            if grid[j][k]["right"] > 1 and "is_across" in grid[j][k]:
                 write_clue(grid, clues, j, k, "right")
 
     grid_as_json = {
@@ -254,28 +262,29 @@ def get_grid_as_json(grid):
 
 
 def write_clue(grid, clues, j, k, direction):
-    '''
+    """
     Add a clue to the clues array
 
-    Parameters: 
+    Parameters:
     grid: An array representation of the grid with dictionary
           fields for each cell
     clues: The clues array so far (ndarray)
     j: The current cell x-coordinate (int)
     k: The current cell y-coordinate (int)
     direction: The current clue direction (String)
-    '''
+    """
 
     direction_number = 1 if direction == "down" else 0
 
     if "clue_number" in grid[j][k]:
-        clues.append({
-            "number": grid[j][k]["clue_number"],
-            "direction": direction_number,
-            "text": "TODO",
-            "totalLength": grid[j][k][direction],
-            "lengths": [grid[j][k][direction]],
-            "x": k,
-            "y": j,
-        })
-
+        clues.append(
+            {
+                "number": grid[j][k]["clue_number"],
+                "direction": direction_number,
+                "text": "TODO",
+                "totalLength": grid[j][k][direction],
+                "lengths": [grid[j][k][direction]],
+                "x": k,
+                "y": j,
+            }
+        )
--- cryptic_solver/image_processing/transform_image.py
+++ cryptic_solver/image_processing/transform_image.py
@@ -4,24 +4,27 @@ import numpy as np
 import PIL.Image
 import pytesseract
 import re
+
 # pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 # !!! Tesseract command different on every machine
 
-def black_treshold(r, g, b):
-    return r < 100 and g < 100 and b < 100
 
-def thicken_black_contours(cv2_img):
-    '''
+def black_treshold(r, g, b):
+    return r < 120 and g < 120 and b < 120
+
+
+def thicken_black_contours(cv2_img, neighbourhood_size=4):
+    """
     Makes the black lines in the image thicker so that
     they could be more easily recognized.
 
-    Parameters: 
+    Parameters:
     image_path: The string path of the image
 
     Return:
     img2: Image with thickened black contours
-    '''
- 
+    """
+
     # Load the image and a copy of it to modify as a PIL images
     cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     img = PIL.Image.fromarray(cv2_img)
@@ -30,17 +33,16 @@ def thicken_black_contours(cv2_img):
     pix = img.load()
     pix2 = img2.load()
 
-
     # Change the neighbourhood pixels of the black pixels to
     # black where there is threshold for classifying as black
-    for x in range(5, width - 5):
-        for y in range(5, height - 5):
+    for x in range(neighbourhood_size, width - neighbourhood_size):
+        for y in range(neighbourhood_size, height - neighbourhood_size):
             (r, g, b) = pix[x, y]
             if black_treshold(r, g, b):
                 pix2[x, y] = (0, 0, 0)
-                for i in range(5):
-                    for j in range(5):
-                        pix2[x+i, y+j] = (0, 0, 0)
+                for i in range(neighbourhood_size):
+                    for j in range(neighbourhood_size):
+                        pix2[x + i, y + j] = (0, 0, 0)
 
     # Change the non-black pixels to white where there is
     # threshold for classifying as black
@@ -54,25 +56,47 @@ def thicken_black_contours(cv2_img):
     return img2
 
 
+def sort_points(pts):
+
+    pts = np.array([pts[i][0] for i in range(4)])
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    print(rect)
+    return rect
+
+
 def transform_to_cornersangle(image, points):
-    '''
+    """
     Transforms the image to a rectangle with sides parallel
     and perpendicular to the horizontal
 
-    Parameters: 
+    Parameters:
     image: Image data array
     points: 4x2 array that contains the coordinates of the
            corners of the tilted grid
 
     Return:
     img2: Image with thickened black contours
-    '''
+    """
 
     # Get the array of 4 corners of the grid and move a bit towards
     # the corners of the whole image to allow for some white space
     # around the image after cropping
-    corners = np.array([points[0][0], points[3][0], points[2]
-                       [0], points[1][0]], dtype="float32")
+
+    # corners = np.array(
+    #     [points[0][0], points[3][0], points[2][0], points[1][0]], dtype="float32"
+    # )
+
+    corners = sort_points(points)
 
     corners[0][0] -= 50
     corners[0][1] -= 50
@@ -87,6 +111,7 @@ def transform_to_cornersangle(image, points):
     corners[3][1] += 50
 
     (tl, tr, br, bl) = corners
+    print(corners)
 
     # Use the Pythagorean Theorem to find the approximate sides of the
     # tilted grid so that we can use them later to keep the same size
@@ -99,27 +124,28 @@ def transform_to_cornersangle(image, points):
     width = max(int(bottom_width), int(top_width))
 
     # Construct an array with the expected corners of the non-tilted image
-    dst_corners = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1],
-                            [0, height - 1]], dtype="float32")
+    dst_corners = np.array(
+        [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]],
+        dtype="float32",
+    )
 
     transform_matrix = cv2.getPerspectiveTransform(corners, dst_corners)
-    rectangular_image = cv2.warpPerspective(
-        image, transform_matrix, (width, height))
+    rectangular_image = cv2.warpPerspective(image, transform_matrix, (width, height))
 
     return rectangular_image
 
 
 def process_grid_image(cv2_img):
-    '''
-    Detects the corners of the grid and rotates it to a rectangle 
+    """
+    Detects the corners of the grid and rotates it to a rectangle
     with sides parallel and perpendicular to the horizontal
 
-    Parameters: 
+    Parameters:
     path: The path tho the image
 
     Return:
     rectangular_image: The image with non-tilted grid inside
-    '''
+    """
 
     pil_image = thicken_black_contours(cv2_img)
     image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
@@ -133,14 +159,14 @@ def process_grid_image(cv2_img):
     # Turn to grayscale and perform Gaussian blur to find the
     # edges in the grid more easily
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    T, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(thresh, (5, 5), 0)
     edged = cv2.Canny(blur, 75, 200)
 
-    cv2.imwrite("out/edged.png", edged)
+    cv2.imwrite("out/edged.png", blur)
 
     # Find the contours and extract the 5 longest contours
-    contours = cv2.findContours(
-        edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours(blur.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
@@ -153,29 +179,31 @@ def process_grid_image(cv2_img):
             corners = approx_corners
             break
 
+    print(corners[0])
+
     cv2.drawContours(image, [corners], -1, (0, 255, 0), 2)
     cv2.imwrite("out/out.png", image)
 
-    corner_points = corners * \
-        np.shape(original_image)[0] / (1.0 * resize_height)
-    rectangular_image = transform_to_cornersangle(
-        original_image, corner_points)
+    corner_points = corners * np.shape(original_image)[0] / (1.0 * resize_height)
+
+    print(corner_points)
+    rectangular_image = transform_to_cornersangle(original_image, corner_points)
     cv2.imwrite("out/warped.png", rectangular_image)
 
     return rectangular_image
 
+
 # Takes a cv2 image
 def transform_text_image(image):
     print(pytesseract.image_to_osd(image))
-    angle = 360 - int(re.search('(?<=Rotate: )\d+', pytesseract.image_to_osd(image)).group(0))
+    angle = 360 - int(
+        re.search("(?<=Rotate: )\d+", pytesseract.image_to_osd(image)).group(0)
+    )
     height, width = image.shape[:2]
 
     # Perform the rotation
     M = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1.0)
     rotated = cv2.warpAffine(image, M, (width, height))
-    
+
     cv2.imwrite("out/rotated.png", rotated)
     return rotated
-
-
-
