51c2f55ba62a3f11d10aca06edd48898d605273b
ztm19 <67147682+ztm19@users.noreply.github.com>
2021-11-17 17:43:50 +0200

    Process blurred tilted image (#52)
    * Increased the contrast of the image to make black lines more visible.
    
    * Grid preprocessing - fixing tilt, contrast, thickening contours
    
    Co-authored-by: ztm19 <zlatinkaaaaaaaaaa@gmail.com>
    
    * Add comments and clean up functions and variables in transform_image.py.
    
    * Add comments, clean up grid_recognition.py and change identation of transform_image.py
    
    * Clean up functions, add consistent commenting in image_recognition and text_recognition.
    
    * Minor comment fix in grid_recognition
    
    * Changed grid image processing to take a cv2 image, integrated with get_grid_from_image
    
    * Try to fix failing pipeline
    
    * Update requirements.txt
    
    * Tuned grid recognition
    
    * Cleaning up transform image
    
    * Match the image_recognition in master
    
    * Fix failing pipeline
    
    * Another fix of the pipeline failing
    
    * Removed comment for tesseract cmd
    
    Co-authored-by: Zlatina Mileva <zlatina.mileva19@imperial.ac.uk>
    Co-authored-by: Georgi Angelov <georgi@Georgis-MacBook-Air.local>
    Co-authored-by: Guolin Yang <guolin.yang13@gmail.com>

---
+++ cryptic_solver/image_processing/image_recognition.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import base64
+from cryptic_solver.image_processing.grid_recognition import get_grid_from_image
+from cryptic_solver.image_processing.text_recognition import read_text
+
+# Combine grid and text recognition to produce one 
+# object with all crossword data
+def recognize_image(b64_grid, b64_across, b64_down):
+
+    grid = b64ToMatrix(b64_grid)
+    across = b64ToMatrix(b64_across)
+    down = b64ToMatrix(b64_down)
+
+    grid_dict = get_grid_from_image(grid)
+    across_dict = read_text(across)
+    down_dict = read_text(down)
+
+    grid_clues = grid_dict["clues"]
+
+    for clue in down_dict:
+      for grid_clue in grid_clues:
+          if grid_clue["direction"] == 1 and grid_clue["number"] == clue.get("number", -1):
+            grid_clue["text"] = clue.get("text", "TODO")
+            grid_clue["lengths"] = list(map(int, clue.get("lengths", [0])))
+
+    for clue in across_dict:
+      for grid_clue in grid_clues:
+          if grid_clue["direction"] == 0 and grid_clue["number"] == clue.get("number", -1):
+            grid_clue["text"] = clue.get("text", "TODO")
+            grid_clue["lengths"] = list(map(int, clue.get("lengths", [0])))
+
+    grid_dict["clues"].sort(key=lambda c: c["number"])
+
+    return grid_dict
+
+def b64ToMatrix(b64_image):
+    im_bytes = base64.b64decode(b64_image)
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8) 
+    return cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+
