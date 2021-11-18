d505fd7876675be151c9d5ccdd04f2aecd0f8d73
Xiaoning He <xhe123@hotmail.co.uk>
2021-11-18 00:27:19 +0000

    Add confidences to image processing

--- cryptic_solver/image_processing/text_recognition.py
+++ cryptic_solver/image_processing/text_recognition.py
@@ -3,7 +3,9 @@ import cv2
 import re
 import easyocr
 from functools import reduce
-from transform_image import transform_text_image
+# TODO change when merged to master
+if __name__ != "__main__":
+    from cryptic_solver.image_processing.transform_image import transform_text_image
 
 reader = easyocr.Reader(['en'])
 
@@ -47,13 +49,47 @@ def postprocess_text(text):
     # Take care of cases where the lengths of a two-word clue have been stuck together
     for i in range(len(replaced_text)-2):
         if replaced_text[i] == "(" and \
-           replaced_text[i + 1] > "1" and \
-           replaced_text[i + 1] <= "9" and \
-           replaced_text[i + 2] >= "1" and \
-           replaced_text[i + 2] <= "9":
+            "1" < replaced_text[i + 1] <= "9" and \
+           "1" <= replaced_text[i + 2] <= "9":
             replaced_text = replaced_text[:i + 2] + "," + replaced_text[i + 2:]
 
     return replaced_text
+
+
+def postprocess_text_with_conf(text):
+    '''
+    Changes the misrecognized characters to the expected ones
+
+    Parameters: 
+    text: String
+
+    Return:
+    replaced_text: String
+    '''
+    character_replacements = {
+        "°": " ",
+        "©": "(5)",
+        "|": "I",
+        "®": "(5",
+        "@": "(4)",
+    }
+
+    replaced_texts = []
+
+    for replaced_text, conf, line_no in text:
+        for (k, r) in character_replacements.items():
+            replaced_text = replaced_text.replace(k, r)
+
+        # Take care of cases where the lengths of a two-word clue have been stuck together
+        for i in range(len(replaced_text)-2):
+            if replaced_text[i] == "(" and \
+                "1" < replaced_text[i + 1] <= "9" and \
+                    "1" <= replaced_text[i + 2] <= "9":
+                replaced_text = replaced_text[:i +
+                                              2] + "," + replaced_text[i + 2:]
+        replaced_texts.append((replaced_text, conf, line_no))
+
+    return replaced_texts
 
 
 def make_text(a, b):
@@ -74,35 +110,81 @@ def read_text_multimodal(image_data, ocr):
     clues: An array of clues with their text, number, lengths
     '''
 
-    processed = preprocess(image_data)
+    custom_config = r"--oem 3 --psm 6"
+    if ocr == "original":
+        # Preprocessing: returns preprocessed image
+        processed = preprocess_original(image_data)
 
-    if ocr == "tesseract":
-        custom_config = r"--oem 3 --psm 6"
-        text = pytesseract.image_to_string(processed, config=custom_config)
+        # OCR processing: extract (string, conf, line number) from image
+        text = pytesseract.image_to_data(
+            processed, config=custom_config, output_type='data.frame')
+        text = text[text.conf > 0]
+        text = [tuple(x) for x in text[text.conf != -1]
+                [['text', 'conf', 'line_num']].to_numpy()]
+
+        # Postprocessing: filters badly processed text
+        postprocessed_text = postprocess_text_with_conf(text)
+
+        # Parse into clues: number, length, text and conf
+        clues = parse_ocr_with_conf(postprocessed_text)
+
+        print("\n\nRead Text Original\n")
+    elif ocr == "tesseract":
+        # Preprocessing
+        processed = preprocess(image_data)
+
+        # OCR processing
+        text = pytesseract.image_to_data(
+            processed, config=custom_config, output_type='data.frame')
+        text = [tuple(x) for x in text[text.conf != -1]
+                [['text', 'conf', 'line_num']].to_numpy()]
+
+        # Postprocessing
+        postprocessed_text = postprocess_text_with_conf(text)
+
+        # Parse into clues
+        clues = parse_ocr_with_conf(postprocessed_text)
+
+        print("\n\nRead Text New Preprocess\n")
     elif ocr == "easy_ocr":
-        text = reader.readtext(processed, detail=0)
-        text = reduce(make_text, text)
+        # Preprocessing
+        processed = preprocess(image_data)
 
-    text = pytesseract.image_to_data(
-        processed, config=custom_config, output_type='data.frame')
-    text = [tuple(x) for x in text[text.conf != -1]
-            [['conf', 'text']].to_numpy()]
-    lines = text.groupby(['page_num', 'block_num', 'par_num', 'line_num'])['text'] \
-        .apply(lambda x: ' '.join(list(x))).tolist()
-    confs = text.groupby(['page_num', 'block_num', 'par_num', 'line_num'])[
-        'conf'].mean().tolist()
-    line_conf = []
-    for i in range(len(lines)):
-        if lines[i].strip():
-            line_conf.append((lines[i], round(confs[i], 3)))
-    print("Lines")
-    for i in line_conf:
-        print(i)
+        # OCR processing
+        text = [x[-2:] + tuple([[x[0][0][1], x[0][2][1]]])
+                for x in reader.readtext(processed)]
+        text_with_line_no = []
+        last_topdown = (-2, -1)
+        line_no = 0
+        for word, conf, curr_topdown in text:
+            if not sufficiently_overlap(last_topdown, curr_topdown):
+                line_no += 1
+            text_with_line_no.append((word, conf, line_no))
+            last_topdown = curr_topdown
+        text = text_with_line_no
 
-    text = pytesseract.image_to_string(processed, config=custom_config)
-    preprocessed_text = postprocess_text(text)
-    clues = parse_ocr(preprocessed_text)
+        # Postprocessing
+        postprocessed_text = postprocess_text_with_conf(text)
+
+        # Parse into clues
+        clues = parse_ocr_with_conf(postprocessed_text)
+
+        print("\n\nRead Text Easy OCR\n")
     return clues
+
+
+def sufficiently_overlap(last_topdown, curr_topdown):
+    last_width = last_topdown[1] - last_topdown[0]
+    curr_width = curr_topdown[1] - curr_topdown[0]
+    if last_topdown[1] < curr_topdown[0] or last_topdown[0] > curr_topdown[1]:
+        return False
+    if last_topdown[1] >= curr_topdown[0]:
+        inner_width = last_topdown[1] - curr_topdown[0]
+        return inner_width / curr_width > 0.75 or inner_width / last_width > 0.75
+    if last_topdown[0] <= curr_topdown[1]:
+        inner_width = curr_topdown[1] - last_topdown[0]
+        return inner_width / curr_width > 0.75 or inner_width / last_width > 0.75
+    return False
 
 
 def parse_ocr(text):
@@ -157,6 +239,55 @@ def parse_ocr(text):
     return clues
 
 
+def parse_ocr_with_conf(text):
+    clues = []
+    clue = {}
+    clue_text = ""
+
+    line = ""
+    line_builder = []
+    last_line_no = 1
+    for i, (string, conf, line_no) in enumerate(text):
+        # if not gone onto new line or reached last parsed text
+        if not (line_no != last_line_no or i == len(text)-1):
+            line_builder.append(string)
+            continue
+
+        last_line_no = line_no
+        if i == len(text)-1:
+            line_builder.append(string)
+        line = " ".join(line_builder)
+        line_builder = [string]
+
+        words = line.split()
+        if len(words) == 0:
+            continue
+
+        # When a line starts with a number, it's a new clue
+        # (add the currently building clue to clues, make a new one)
+        if re.match(r"\d+.*", words[0]) and len(words) > 1:
+            if clue:
+                clue["text"] = clue_text.strip()
+                clues.append(clue)
+            clue = {}
+            clue_text = ""
+            clue["number"] = get_int(words[0])
+            del words[0]
+
+        # If line ends with a number (excluding brackets)
+        # then that line ending is the solution pattern
+        if re.match("(\()?\d+((\.,-)?\d+)*(\))?", words[-1]):
+            lengths_string = words[-1].replace(".", ",")[1:-1]
+            lengths = re.split(",|-", lengths_string)
+            clue["lengths"] = lengths
+
+        clue_text += " " + " ".join(words)
+
+    clue["text"] = clue_text.strip()
+    clues.append(clue)
+    return clues
+
+
 def read_text(image_data):
     '''
     Extract the text from the given image
@@ -167,15 +298,7 @@ def read_text(image_data):
     Return:
     clues: An array of clues with their text, number, lengths
     '''
-    transformed = transform_text_image(image_data)
-    gray = preprocess_original(transformed)
-
-    custom_config = r"--oem 3 --psm 6"
-    text = pytesseract.image_to_string(gray, config=custom_config)
-    postprocessed_text = postprocess_text(text)
-    clues = parse_ocr(postprocessed_text)
-
-    return clues
+    return read_text_multimodal(image_data, "original")
 
 
 def distance_transform(image):
@@ -219,6 +342,8 @@ def get_int(word):
 
 
 if __name__ == "__main__":
+    from transform_image import transform_text_image
     img = cv2.imread("inputs/pics/clear/across1.jpg")
-    print(read_text(img))
-    print(read_text_multimodal(img))
+    print(read_text_multimodal(img, "original"))
+    print(read_text_multimodal(img, "tesseract"))
+    print(read_text_multimodal(img, "easy_ocr"))
