3e61b6420f78a5425cb1d97cd1deec9150996746
Georgi Angelov <georgi@Georgis-MacBook-Air-3.local>
2021-11-25 02:24:45 +0200

    updated tests to use new format of pattern

--- cryptic_solver/tests.py
+++ cryptic_solver/tests.py
@@ -3,25 +3,25 @@ import unittest
 
 class DictTests(unittest.TestCase):
     def test_one(self):
-        pattern = {"0": 'A', "2":'E', "4":'A'} #A_E_A__
+        pattern = "A_E_A__" #{"0": 'A', "2":'E', "4":'A'} 
 
         self.assertEqual(get_candidates(pattern, word_length=7), ['abeyant', 'acerata', 'acerate', 'acetals', 'acetary', 'acetars', 'acetate', 'adenase', 'agelast', 'agelaus', 'alegars', 'aleyard', 'alemana', 'ameland', 'amenage', 'amesace', 'anelace', 'apetaly', 'arecain', 'arefact', 'avenage', 'avenant', 'avenary', 'average', 'aweband', 'azelaic', 'azelate'])
 
     def test_two(self):
-        pattern = {0: 'A', 2:'E', 4:'A'} #A_E_A__
+        pattern = "A_E_A__" #{0: 'A', 2:'E', 4:'A'} 
 
         self.assertEqual(get_candidates(pattern, word_length=7), ['abeyant', 'acerata', 'acerate', 'acetals', 'acetary', 'acetars', 'acetate', 'adenase', 'agelast', 'agelaus', 'alegars', 'aleyard', 'alemana', 'ameland', 'amenage', 'amesace', 'anelace', 'apetaly', 'arecain', 'arefact', 'avenage', 'avenant', 'avenary', 'average', 'aweband', 'azelaic', 'azelate'])
 
 class MatchingTests(unittest.TestCase):
     def test_one_solution(self):
-        pattern = {0: 'A', 2:'E', 4:'A'} #A_E_A__
+        pattern = "A_E_A__" #{0: 'A', 2:'E', 4:'A'}
         list = ['AVERAGE', 'ACADEMY', 'ADDRESS', 'ACCUSED', 'ABILITY']
 
         self.assertEqual(matching(pattern, list), ['AVERAGE'])
 
 
     def test_more_solutions(self):
-        pattern = {0: 'B', 2:'B', 4:'E'} #B_B_E_
+        pattern = "B_B_E_" #{0: 'B', 2:'B', 4:'E'}
         list = ['BABBLE', 'BABIED', 'BABIES', 'BABOON']
 
         self.assertEqual(matching(pattern, list), ['BABIED', 'BABIES'])
